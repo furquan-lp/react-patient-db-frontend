74bf42cbcc79a6a8a87669f6256cd8f7b9b46972
Syed F Ahmad <furquan@protonmail.com>
2023-10-19 22:11:56 +0530

    Added the rest of the form and adjusted gaps

--- react-patients/src/components/ViewAdd.tsx
+++ react-patients/src/components/ViewAdd.tsx
@@ -6,7 +6,7 @@ export default function ViewAdd() {
   return (
     <Box sx={{ my: 4, gap: 2, display: 'flex', flexDirection: 'column' }}>
       <Card sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', p: 2, height: 1 / 5, width: 700 }}>
-        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, }}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 3, }}>
           <Box sx={{ display: 'flex', gap: 4 }}>
             <TextField id="outlined-basic" label="Patient ID" variant="outlined" InputProps={{ readOnly: true }}
               defaultValue='ID2023' />
@@ -16,20 +16,33 @@ export default function ViewAdd() {
           <Box sx={{ display: 'flex', gap: 4 }}>
             <TextField sx={{ width: '8ch' }} id="outlined-basic" label="Age" variant="outlined" />
             <TextField id="outlined-basic" label="Phone Number" variant="outlined" InputProps={{
-              startAdornment: <InputAdornment position="start">+91</InputAdornment>,
+              startAdornment: <InputAdornment position="start">+91</InputAdornment>
             }} />
             <TextField sx={{ width: '42ch' }} id="outlined-basic" label="Address" variant="outlined" />
           </Box>
         </Box>
       </Card>
       <Card sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', p: 2, height: 1 / 5, width: 700 }}>
-        <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', gridTemplateRows: 'repeat(2, 1fr)', gap: 2 }}>
+        <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', gridTemplateRows: 'repeat(2, 1fr)', gap: 3 }}>
           <TextField id="outlined-basic" label="Prescription" variant="outlined" />
           <TextField id="outlined-basic" label="Dose" variant="outlined" />
           <LocalizationProvider dateAdapter={AdapterDayjs}>
             <DatePicker label="Visit Date" />
             <DatePicker label="Next Visit" />
           </LocalizationProvider>
+        </Box>
+      </Card>
+
+      <Card sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', p: 2, height: 1 / 5, width: 700 }}>
+        <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', gridTemplateRows: 'repeat(2, 1fr)', gap: 3 }}>
+          <TextField id="outlined-basic" label="Physician ID" variant="outlined" />
+          <TextField id="outlined-basic" label="Physician Name" variant="outlined" />
+          <TextField id="outlined-basic" label="Phone Number" variant="outlined" InputProps={{
+            startAdornment: <InputAdornment position="start">+91</InputAdornment>
+          }} sx={{ width: '25ch' }} />
+          <TextField id="outlined-basic" label="Bill" variant="outlined" InputProps={{
+            startAdornment: <InputAdornment position="start">₹</InputAdornment>
+          }} sx={{ width: '12ch' }} />
         </Box>
       </Card>
     </Box>
