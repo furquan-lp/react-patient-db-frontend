a55157c4a8d676f82ac906686b2bca9eaf142f1d
Syed F Ahmad <furquan@protonmail.com>
2023-10-19 21:31:05 +0530

    Added ViewAdd

--- react-patients/src/App.tsx
+++ react-patients/src/App.tsx
@@ -1,9 +1,12 @@
 import SideBar from "./components/SideBar";
+import ViewAdd from "./components/ViewAdd";
 
 function App() {
+  const l = 0;
   return (
     <main className='app-main'>
       <SideBar />
+      <ViewAdd />
     </main>
   );
 }
